1acafc458ef4a1ef30ef45c0bec6f6109a6246b7
Thiago Peixoto dos Santos <thiago.santos.ts3@csn.com.br>
2025-09-04 14:53:06 -0300

    ultimas migrations

---
+++ src/Infra/Migrations/20250904174917_v1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class v1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Patients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PatientName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patients", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Emotionals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PatientId = table.Column<int>(type: "int", nullable: false),
+                    GeneratedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    BeginPeriodDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndPeriodDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ResumeText = table.Column<string>(type: "text", nullable: false),
+                    Prompt = table.Column<string>(type: "text", nullable: false),
+                    ModelIa = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Emotionals", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Emotionals_Patients_PatientId",
+                        column: x => x.PatientId,
+                        principalTable: "Patients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Emotionals_PatientId",
+                table: "Emotionals",
+                column: "PatientId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Emotionals");
+
+            migrationBuilder.DropTable(
+                name: "Patients");
+        }
+    }
+}
