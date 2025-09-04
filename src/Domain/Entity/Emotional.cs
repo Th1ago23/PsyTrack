aaca177522843ef70f4395db95ce8e2544975db0
Thiago Peixoto dos Santos <thiago.santos.ts3@csn.com.br>
2025-09-04 09:10:50 -0300

    Mapeando entidades

---
+++ src/Domain/Entity/Emotional.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entity;
+
+public class Emotional
+{
+    public int Id { get; private set; }
+    public int PatientId { get; set; }
+    public required Patient Patient { get; set; }
+    public string Resume { get; set; } = string.Empty;
+
+    
+}
---
+++ src/Domain/Entity/Patient.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Entity;
+
+public class Patient
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public ICollection<Emotional> Emotional { get; set; } = new List<Emotional>();
+}
