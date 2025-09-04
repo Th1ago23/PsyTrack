54232332b006a994536ad5835ca2e9a821e05eaf
Thiago Peixoto dos Santos <thiago.santos.ts3@csn.com.br>
2025-09-04 14:52:22 -0300

    Nova config

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,3 +1,6 @@
+using Infra.Config;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var configuration = builder.Configuration;
+
+string connectionString = configuration.GetConnectionString("DbConnection");
+
+builder.Services.AddDbContext<DbConfig>(options =>
+    options.UseSqlServer(connectionString,
+        b => b.MigrationsAssembly("Infra")));
 
 var app = builder.Build();
 
