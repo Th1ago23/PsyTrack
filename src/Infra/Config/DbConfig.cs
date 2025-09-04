a4c000dc345ca5562bea240fad01d689a84a6eeb
Thiago Peixoto dos Santos <thiago.santos.ts3@csn.com.br>
2025-09-04 14:52:57 -0300

    Alterei pro entity

---
+++ src/Infra/Config/DbConfig.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
+
+namespace Infra.Config;
+
+public class DbConfig:DbContext
+{
+    public DbConfig(DbContextOptions<DbConfig> options) : base(options) { }
+
+    public DbSet<Emotional> Emotionals { get; set; }
+    public DbSet<Patient> Patients { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Patient>()
+            .HasMany(p => p.Emotional)
+            .WithOne(e => e.Patient)
+            .HasForeignKey(e => e.PatientId);
+
+    }
+}
